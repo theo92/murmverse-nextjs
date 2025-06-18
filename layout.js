9cab480ecd5d894c0a1f9457bc6cd5b91d8221d9
cappy <43281598+theo92@users.noreply.github.com>
2025-06-18 11:04:59 +0200

    Delete layout.js

--- layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Murmverse" };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0, backgroundColor: 'black', color: 'white' }}>{children}</body>
-    </html>
-  );
-}
